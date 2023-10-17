6ab7c71a0e1e5ba652c40af5faa6d92c00d87656
WooJin Park(Ader) <ak2j38@gmail.com>
2023-10-17 23:38:10 +0900

    MdcFilter uuid 기반 및 체이닝을 통해 로그트레이싱 용이하도록 수정

--- src/main/kotlin/com/ader/simplepayment/config/MdcFilter.kt
+++ src/main/kotlin/com/ader/simplepayment/config/MdcFilter.kt
@@ -1,5 +1,6 @@
 package com.ader.simplepayment.config
 
+import mu.KotlinLogging
 import org.slf4j.MDC
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
@@ -7,14 +8,26 @@ import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebFilter
 import org.springframework.web.server.WebFilterChain
 import reactor.core.publisher.Mono
+import reactor.util.context.Context
 import java.util.UUID
+
+private val logger = KotlinLogging.logger {}
 
 @Component
 @Order(1)
 class MdcFilter: WebFilter {
   override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
-    val uuid = "${UUID.randomUUID()}"
-    MDC.put("txId", uuid)
-    return chain.filter(exchange)
+    val uuid = exchange.request.headers["x-txid"]?.getOrNull(0) ?: "${UUID.randomUUID()}".replace("-","")
+    MDC.put("txid", uuid)
+    logger.info{ exchange.request.let { request ->
+      "uri: [${request.method}] ${request.path}, ip: ${request.remoteAddress}${
+        if(request.queryParams.isEmpty()) "" else ", query-param: ${request.queryParams}"
+      }"
+    }}
+    return chain.filter(exchange).doOnError {
+      ErrorConfig.mapTxid[exchange.request.id] = uuid
+    }.contextWrite {
+      Context.of("txid", uuid)
+    }
   }
 }
